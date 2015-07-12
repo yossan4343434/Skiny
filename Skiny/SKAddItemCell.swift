150af4c4247eb51290bb018b5b22b17b20f3d86f
yossan4343434 <yossan4343434@gmail.com>
2015-07-12 13:36:14 +0900

    Add SKAddItemViewController

---
+++ Skiny/SKAddItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  SKAddItemCell.swift
+//  Skiny
+//
+//  Created by Yoshiyuki Kuga on 2015/07/11.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKAddItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Skiny/SKAddItemViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SKAddItemViewController.swift
+//  Skiny
+//
+//  Created by Yoshiyuki Kuga on 2015/07/11.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKAddItemViewController: UIViewController {
+
+    @IBOutlet weak var selectCategoryView: UIView!
+    @IBOutlet weak var selectBrandView: UIView!
+    @IBOutlet weak var selectItemView: UIView!
+    @IBOutlet weak var submitView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
