f26d2b2e5c69ac807e79ec8d421b5269cb8e5792
yossan4343434 <yossan4343434@gmail.com>
2015-07-14 00:49:26 +0900

    Cleanup some code

--- Skiny/SKUserItemListViewController.swift
+++ Skiny/SKUserItemListViewController.swift
@@ -12,7 +12,7 @@ class SKUserItemListViewController: UIViewController, UITableViewDelegate, UITab
 
     @IBOutlet weak var userItemListTableView: UITableView!
 
-    let userItem = [["category": "lotion", "brand": NSLocalizedString("muji", comment: "muji"), "name": "化粧水・敏感肌用 高保湿タイプ"], ["category": "化粧水", "brand": "無印良品", "name": "化粧水・敏感肌用 高保湿タイプ"], ["category": "化粧水", "brand": "無印良品", "name": "化粧水・敏感肌用 オーガニック化粧水 高保湿タイプ"]]
+    var sampleData: NSArray = NSArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,16 @@ class SKUserItemListViewController: UIViewController, UITableViewDelegate, UITab
 
         self.userItemListTableView.delegate = self
         self.userItemListTableView.dataSource = self
+
+        loadData()
+    }
+
+    func loadData() {
+        if let path = NSBundle.mainBundle().pathForResource("sample", ofType: "json") {
+            if let jsonData = NSData(contentsOfFile: path) {
+                sampleData = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments, error: nil) as! NSArray
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,15 +43,18 @@ class SKUserItemListViewController: UIViewController, UITableViewDelegate, UITab
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return userItem.count
+        return sampleData.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: SKUserItemListCell = tableView.dequeueReusableCellWithIdentifier("UserItemListCell", forIndexPath: indexPath) as! SKUserItemListCell
-        cell.userItemImageView.image = UIImage(named: "dummy.jpg")
-        cell.userItemCategoryLabel.text = userItem[indexPath.row]["category"]
-        cell.userItemBrandLabel.text = userItem[indexPath.row]["brand"]
-        cell.userItemNameLabel.text = userItem[indexPath.row]["name"]
+
+        cell.userItemCategoryLabel.text = sampleData[indexPath.row]["category"] as! String
+        cell.userItemBrandLabel.text = sampleData[indexPath.row]["brand"] as! String
+        cell.userItemNameLabel.text = sampleData[indexPath.row]["name"] as! String
+        let imageName = sampleData[indexPath.row]["image"] as! String
+        cell.userItemImageView.image = UIImage(named: imageName)
+
         return cell
     }
 
@@ -50,7 +63,7 @@ class SKUserItemListViewController: UIViewController, UITableViewDelegate, UITab
     }
 
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("hello")
+        print("item tapped")
     }
     
 }
