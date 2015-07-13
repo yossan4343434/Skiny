600d6fba2107a03250b33f7a32678b0d51fa4d7c
yossan4343434 <yossan4343434@gmail.com>
2015-07-13 16:12:22 +0900

    Localize Skiny

---
+++ Skiny/SKItemDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SKItemDetailViewController.swift
+//  Skiny
+//
+//  Created by Yoshiyuki Kuga on 2015/07/12.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
